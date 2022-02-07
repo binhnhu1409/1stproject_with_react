73804edd5627b15ae9dc964f0aa7786c8658d925
binhnhu1409 <binhnhu1409@gmail.com>
2022-02-07 12:41:55 +0200

    connect todolist to the Redux store

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { createTodo } from './actions';
 import './NewTodoForm.css';
+
 const NewTodoForm = ({ todos, onCreatePressed }) => {
     const [inputValue, setInputValue] = useState('');
     return (
@@ -27,10 +28,13 @@ const NewTodoForm = ({ todos, onCreatePressed }) => {
         </div>
     );
 };
+
 const mapStateToProps = state => ({
     todos: state.todos,
 });
+
 const mapDispatchToProps = dispatch => ({
     onCreatePressed: text => dispatch(createTodo(text)),
 });
+
 export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
